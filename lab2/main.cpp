a7d79bcd236fba25b6969ba7e4db9a79ea2a3da3
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-02-23 11:58:23 +0200

    Add classes objects and test functions to main

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
+#include "Menu.h"
+#include "Bar.h"
+#include "Delivery.h"
 
+int main() {
+Menu dish1("Steak",100, 300);
+Bar cocktail1("Pina Colada", 100, "1+1=3");
+Delivery order1(true, 50);
+
+dish1.display();
+cocktail1.display();
+order1.displayInfo();
     return 0;
 }
