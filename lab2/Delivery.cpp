eeb7ec64d5dd13ca07ac1278c62981b58475ca89
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-02-23 10:23:51 +0200

    Implement Delivery class

--- lab2/Delivery.cpp
+++ lab2/Delivery.cpp
@@ -1,5 +1,18 @@
-//
-// Created by user on 23.02.2024.
-//
-
 #include "Delivery.h"
+#include <iostream>
+using namespace std;
+
+Delivery::Delivery()
+        : Delivery{false, 0}{}
+Delivery::Delivery(bool available, double deliveryPrice)
+        :isAvailable{available}, price{deliveryPrice}{}
+Delivery::~Delivery(){};
+
+void Delivery::setAvailability(bool available) {
+    isAvailable = available;
+}
+void Delivery::displayInfo() {
+    cout << "Delivery Information:\n";
+    cout << "Available: " << (isAvailable ? "Yes" : "No") << endl;
+    cout << "Price: " << price << " USD" << endl;
+}
