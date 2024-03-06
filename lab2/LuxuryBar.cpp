8403bda97fc92491e8b50d565967835accacd488
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-03-06 17:08:09 +0200

    Implement LuxuryBar class

--- lab2/LuxuryBar.cpp
+++ lab2/LuxuryBar.cpp
@@ -1,5 +1,19 @@
-//
-// Created by user on 06.03.2024.
-//
-
 #include "LuxuryBar.h"
+#include "Bar.h"
+#include <iostream>
+using namespace std;
+
+LuxuryBar::LuxuryBar(string newName, double newPrice, string newSpecial, bool newPremiumQuality, string newOrigin)
+        : Bar{newName, newPrice, newSpecial,} premiumQuality{newPremiumQuality};, origin{newOrigin} {}
+
+LuxuryBar::LuxuryBar(const LuxuryBar &other)
+        : Bar{other}, premiumQuality{other.premiumQuality}, origin{other.origin}{}
+~LuxuryBar::LuxuryBar(){};
+
+void LuxuryBar::display() {
+    cout << "Name: " << name << endl;
+    cout << "Price: " << price << " USD" << endl;
+    cout << "Special: " << special << endl;
+    cout << "Premium Quality: " << (premiumQuality ? "Yes" : "No") << endl;
+    cout << "Origin: " << origin << endl;
+}
