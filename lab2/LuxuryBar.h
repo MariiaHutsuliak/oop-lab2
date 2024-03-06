b6c5d076b6a3725a07a60e4358e040b9da1e993d
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-03-06 17:06:56 +0200

    Add child class for parent Bar class - LuxuryBar

--- lab2/LuxuryBar.h
+++ lab2/LuxuryBar.h
@@ -1,12 +1,20 @@
-//
-// Created by user on 06.03.2024.
-//
-
 #ifndef LAB2_LUXURYBAR_H
 #define LAB2_LUXURYBAR_H
+#include "Bar.h"
 
+class LuxuryBar: public Bar {
+public:
+    bool premiumQuality;
+    string origin;
 
-class LuxuryBar {
+    LuxuryBar (string newName = "None", double newPrice = 0, string newSpecial = "None",
+               bool newPremiumQuality = false, string newOrigin = "None");
+
+    LuxuryBar(const LuxuryBar &other);
+
+    ~LuxuryBar();
+
+    void display();
 
 };
 
