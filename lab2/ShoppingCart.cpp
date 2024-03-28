e4b17f215182a2532bda5c8ecfd7ee669b607a2b
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-03-28 23:12:03 +0200

    Implement class ShoppingCart

--- lab2/ShoppingCart.cpp
+++ lab2/ShoppingCart.cpp
@@ -1,5 +1,24 @@
-//
-// Created by user on 28.03.2024.
-//
-
 #include "ShoppingCart.h"
+using namespace std;
+#include <iostream>
+ShoppingCart::ShoppingCart() : totalCost(0) {}
+
+ShoppingCart::~ShoppingCart() {}
+
+void ShoppingCart::addItem(const string& name, double price) {
+    items.push_back(CartItem(name, price));
+    totalCost += price;
+}
+
+void ShoppingCart::displayCart() {
+    cout << "Items in Cart:\n";
+    for (const auto& item : items) {
+        cout << item.name << " - $" << item.price << std::endl;
+    }
+    int pay;
+    cout << "Total cost: $" << totalCost << std::endl;
+    cout << "Waiting for payment: ";
+    cin >> pay;
+    cout << endl << "Thank you for choosing our restaurant!";
+
+}
