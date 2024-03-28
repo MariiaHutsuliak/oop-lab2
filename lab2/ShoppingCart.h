159c5894f1829fbfb65760e3d1e129adb76b7291
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-03-28 23:11:15 +0200

    Add class ShoppingCart

--- lab2/ShoppingCart.h
+++ lab2/ShoppingCart.h
@@ -1,14 +1,18 @@
-//
-// Created by user on 28.03.2024.
-//
-
 #ifndef LAB2_SHOPPINGCART_H
 #define LAB2_SHOPPINGCART_H
-
+#include <vector>
+#include <string>
+#include "CartItem.h"
 
 class ShoppingCart {
-
+private:
+    std::vector<CartItem> items;
+    double totalCost;
+public:
+    ShoppingCart();
+    ~ShoppingCart();
+    void addItem(const std::string& name, double price);
+    void displayCart();
 };
-
 
 #endif //LAB2_SHOPPINGCART_H
